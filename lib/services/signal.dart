96b72377c5a3f7d7eee391c9a8be0069f576e374
Le Nguyen Thai <21127162@student.hcmus.edu.vn>
2025-03-26 18:49:37 +0700

    dfgdfg

---
+++ lib/services/signal.dart
@@ -0,0 +1,39 @@
+import 'dart:developer';
+import 'package:socket_io_client/socket_io_client.dart';
+
+
+const String socketUrl = 'https://mobile-advance-peer-to-peer-backend.onrender.com';
+class SignalService {
+  // instance of Socket
+  Socket? socket;
+
+  SignalService._();
+  static final instance = SignalService._();
+
+  init({required String callerId}) {
+    // init Socket
+    socket = io(socketUrl, {
+      "transports": ['websocket'],
+      "query": {"callerId": callerId}
+    });
+
+    // listen onConnect event
+    socket!.onConnect((data) {
+      print("Socket connected !!");
+    });
+
+    // listen onConnectError event
+    socket!.onConnectError((data) {
+      print("Connect Error $data");
+    });
+
+    // connect socket
+    socket!.connect();
+  }
+}
+
+
+void main() {
+  SignalService.instance.init(callerId: "Sophia");
+  print("done");
+}
